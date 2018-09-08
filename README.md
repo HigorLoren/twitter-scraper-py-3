c27d0a7495acdbe754108b510287d5d11d79708e
HigorLoren <higor.lorenzon@gmail.com>
2018-09-08 18:36:10 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# Twitter Scraper
+
+This is a command-line application written in Python that scrapes and downloads an twitter user's photos (not all :( sorry). Use responsibly.
+
+Install
+-------
+__Just download the main file__
+
+Usage
+-----
+
+To scrape a public user's media:
+```bash
+$ twitter-media-scraper <username>
+```
+*By default, downloaded media will be placed in `<current working directory>/<username_[twitter]>`.*
+
+
+Options
+-------
+
+```
+-d destionation   The Specify destination an it will download in <destionation>/<username_[twitter]>.
+```
+
+License
+-------
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+## Authors
+
+* **Higor Cervelin** - *Initial work* - [HigorCervelin](https://github.com/HigorCervelin)
+
+See also the list of [contributors](https://github.com/HigorCervelin/stalker/contributors) who participated in this project.
---
+++ main.py
@@ -0,0 +1,107 @@
+from bs4 import BeautifulSoup
+from requests import get
+from os import makedirs
+from re import search, findall
+import sys
+
+try:
+    user = sys.argv[1]
+except IndexError:
+    print("You forgot to type the user")
+    exit()
+
+twitter_media_url = f'https://twitter.com/{user}/media'
+try:
+    dest = ''
+    for agv in sys.argv:
+        dest_exist = search(r'-d', str(agv))
+        quiet_exist = search(r'-q', str(agv))
+        if dest_exist:
+            dest_p = sys.argv.index(agv)
+            destination = sys.argv[dest_p+1]
+        if quiet_exist:
+            def logging_scraper(arg):
+                pass
+        if not quiet_exist:
+            logging_scraper = print
+except:
+    pass
+
+try:
+    html_page = get(twitter_media_url)
+    soup = BeautifulSoup(html_page.text, 'html.parser')
+
+    images = soup.find_all('img')
+
+    profile_picture_list = []
+    image_banner_list = []
+    image_banner = ''
+    profile_picture = ''
+
+    for item in images:
+        profile_picture_item = search(r'https://pbs\.twimg\.com/profile_images/.+jpg', str(item))
+        profile_picture_list.append(profile_picture_item)
+        image_banner_item = search(r'https://pbs\.twimg\.com/profile_banners/.+1500x500', str(item))
+        image_banner_list.append(image_banner_item)
+
+    for item in profile_picture_list:
+        if item:
+            profile_picture = item
+
+    for item in image_banner_list:
+        if item:
+            image_banner = item
+
+    private_profile = soup.find('span', class_='ProfileHeaderCard-badges')
+
+    if private_profile:
+        logging_scraper('Private Profile is not supported')
+    if not private_profile:
+        try:
+            path = destination + f'/{user}_[twitter]'
+        except NameError or IndexError:
+            path = f'./{user}_[twitter]'
+
+        try:
+            makedirs(path)
+        except FileExistsError:
+            pass
+
+        try:
+            profile_picture_link = get(profile_picture.group())
+            with open(f"{path}/Profile.jpg", "wb") as code:
+                code.write(profile_picture_link.content)
+            logging_scraper('Profile picture downloaded')
+        except AttributeError:
+            logging_scraper('No Profile Pic')
+
+        try:
+            image_banner_link = get(image_banner.group())
+            with open(f"{path}/Banner.jpg", "wb") as code:
+                code.write(image_banner_link.content)
+            logging_scraper('Banner downloaded')
+        except AttributeError:
+            logging_scraper('No Banner')
+
+        try:
+            makedirs(f"{path}/media/")
+        except FileExistsError:
+            pass
+
+        media_posts = soup.findAll('img', src=True)
+        i = 0
+        count = 0
+        for _ in media_posts:
+            this_is_link = findall(r'https://pbs\.twimg\.com/media/.+.jpg', str(media_posts[i]))
+            if this_is_link:
+                for item in this_is_link:
+                    count = count + 1
+                    logging_scraper(f'Downloading image #{count}')
+                    media_post_link = get(item)
+                    with open(f"{path}/media/{count}.jpg", "wb") as code:
+                        code.write(media_post_link.content)
+            i += 1
+
+    print('Downloaded successfully!')
+except IndexError:
+    print('You must have entered the wrong username.')
